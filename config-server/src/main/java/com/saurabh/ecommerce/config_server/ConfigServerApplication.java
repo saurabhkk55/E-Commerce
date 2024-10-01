47eb06a999f785f5b3753ac27da8b1967326602e
saurabhkk55 <saurabhkumarkardam25@gmail.com>
2024-10-01 12:00:24 +0530

    config server service

--- config-server/src/main/java/com/saurabh/ecommerce/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/saurabh/ecommerce/config_server/ConfigServerApplication.java
@@ -3,11 +3,18 @@ package com.saurabh.ecommerce.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 @SpringBootApplication
+@EnableConfigServer
+@Slf4j
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		log.info("Config server started successfully");
 	}
-
 }
