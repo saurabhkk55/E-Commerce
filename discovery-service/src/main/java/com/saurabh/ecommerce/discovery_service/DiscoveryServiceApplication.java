f7958f7eb1ef21f46083112436140bc6ae7cd334
saurabhkk55 <saurabhkumarkardam25@gmail.com>
2024-10-01 11:44:43 +0530

    discovery service

---
+++ discovery-service/src/main/java/com/saurabh/ecommerce/discovery_service/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.saurabh.ecommerce.discovery_service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class DiscoveryServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		log.info("Eureka Server started successfully");
+	}
+}
